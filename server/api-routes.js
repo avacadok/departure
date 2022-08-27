dcfc8192a8752c5093af7e0865d09023f9582e2d
avacadok <avakuang8@gmail.com>
2022-08-27 00:12:01 -0700

    change routes to get product info and pictures for new-arrivals

--- server/api-routes.js
+++ server/api-routes.js
@@ -4,17 +4,45 @@ const router = express.Router();
 //Database queries
 const db = require('./database');
 
-// display products that are added to the psql within last 3 months (New Arrivals/all products)
+//Display products that are added to the psql within last 3 months (New Arrivals/all products)
 router.get("/new-arrivals", (req, res) => {
   const query = `
-  SELECT * FROM products;
-  `;
+    SELECT 
+      pp.product_id,
+      p.name,
+      p.description,
+      p.price_cents,
+      p.material,
+      p.size,
+      p.category,
+      p.color,
+      ARRAY_AGG(pp.url) AS list_of_pictures
+    FROM products AS p
+    INNER JOIN product_pictures AS pp
+      ON p.id = pp.product_id
+    GROUP BY 
+      1, 2, 3, 4, 5, 6, 7, 8
+    ;`;
   return db
     .query(query)
-    .then(({rows: products}) => {
+    .then(({ rows: products }) => {
       res.json(products);
     })
     .catch(err => console.log("err from /new-arrivals", err))
+});
+
+//Get all the bags from products
+router.get("/bags", (req, res) => {
+  const query = `
+  SELECT * FROM products
+  WHERE category LIKE 'bag';
+  `;
+  return db
+    .query(query)
+    .then(({ rows: bags }) => {
+      res.json(bags);
+    })
+    .catch(err => console.log("err from /bags", err))
 });
 
 module.exports = router;
