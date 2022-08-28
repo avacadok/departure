a897bf6c43dd36a44047801988dd5f817a38f672
avacadok <avakuang8@gmail.com>
2022-08-28 13:36:38 -0700

    add route for accessories and edit route for bags to display the correct info

--- server/api-routes.js
+++ server/api-routes.js
@@ -20,6 +20,8 @@ router.get("/new-arrivals", (req, res) => {
     FROM products AS p
     INNER JOIN product_pictures AS pp
       ON p.id = pp.product_id
+    WHERE
+    DATE_PART('month', p.added_date::date) - DATE_PART('month', CURRENT_DATE::date) <= 3
     GROUP BY 
       1, 2, 3, 4, 5, 6, 7, 8
     ;`;
@@ -34,8 +36,24 @@ router.get("/new-arrivals", (req, res) => {
 //Get all the bags from products
 router.get("/bags", (req, res) => {
   const query = `
-  SELECT * FROM products
-  WHERE category LIKE 'bag';
+  SELECT 
+      pp.product_id,
+      p.name,
+      p.description,
+      p.price_cents,
+      p.material,
+      p.size,
+      p.category,
+      p.color,
+      ARRAY_AGG(pp.url) AS list_of_pictures
+    FROM products AS p
+    INNER JOIN product_pictures AS pp
+      ON p.id = pp.product_id
+    WHERE
+    category LIKE 'bag'
+    GROUP BY 
+      1, 2, 3, 4, 5, 6, 7, 8
+    ;;
   `;
   return db
     .query(query)
@@ -44,5 +62,36 @@ router.get("/bags", (req, res) => {
     })
     .catch(err => console.log("err from /bags", err))
 });
+
+//Get all the accessories from products
+router.get("/accessories", (req, res) => {
+  const query = `
+  SELECT 
+      pp.product_id,
+      p.name,
+      p.description,
+      p.price_cents,
+      p.material,
+      p.size,
+      p.category,
+      p.color,
+      ARRAY_AGG(pp.url) AS list_of_pictures
+    FROM products AS p
+    INNER JOIN product_pictures AS pp
+      ON p.id = pp.product_id
+    WHERE
+    category LIKE 'accessory'
+    GROUP BY 
+      1, 2, 3, 4, 5, 6, 7, 8
+    ;;
+  `;
+  return db
+    .query(query)
+    .then(({ rows: accessories }) => {
+      res.json(accessories);
+    })
+    .catch(err => console.log("err from /accessories", err))
+});
+
 
 module.exports = router;
