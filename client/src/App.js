1240707aa6ab29508e6bd2deef92d8f0e3dfed5a
avacadok <avakuang8@gmail.com>
2022-08-28 15:47:17 -0700

    add new-arrivals, bags, accessories and home routes with react router dom

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import './style/App.css';
-import Navbar from './components/Navbar';
 import LandingPage from './components/LandingPage';
 import Footer from './components/Footer';
+import { Routes, Route, Link } from 'react-router-dom'
+import Bags from './components/Bags';
+import Accessories from './components/Accessories';
+import NewArrivals from './components/NewArrivals';
+
 
 function App() {
-  const [data, setData] = useState({});
+  // const [data, setData] = useState({});
 
   useEffect(() => {
     axios.get('/api/new-arrivals')
@@ -18,16 +22,35 @@ function App() {
 
   useEffect(() => {
     axios.get('/api/bags')
-    .then((res) => {
-      console.log("bags",res.data)
-    })
-    .catch((err) => console.log("err", err));
-  },[]);
+      .then((res) => {
+        console.log("bags", res.data)
+      })
+      .catch((err) => console.log("err", err));
+  }, []);
 
   return (
     <div>
-      <Navbar />
-      <LandingPage />
+      <header>
+        <div className="logo"><Link to='/' className='logo-link'>D E P A W T U R E</Link></div>
+        <nav>
+          <ul className="nav_links" >
+            <li><Link to='/new-arrivals'>New Arrivals</Link></li>
+            <li><Link to='/bags'>Bags</Link></li>
+            <li><Link to='/accessories'>Accessories</Link></li>
+          </ul>
+        </nav>
+        <a className="login" href="#"><button>Log in</button></a>
+      </header>
+
+      <Routes>
+
+        <Route path='/' element={<LandingPage />} />
+        <Route path='/new-arrivals' element={<NewArrivals />} />
+        <Route path='/bags' element={<Bags />} />
+        <Route path='/accessories' element={<Accessories />} />
+
+      </Routes>
+
       <Footer />
     </div>
   );
