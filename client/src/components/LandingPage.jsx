797dadc0c19b8c3dd2f739b0b542b9916519e875
avacadok <avakuang8@gmail.com>
2022-08-28 16:27:58 -0700

    add route for the shop depawture button on main page. to new-arrivals

--- client/src/components/LandingPage.jsx
+++ client/src/components/LandingPage.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 
 const LandingPage = () => {
   return (
@@ -10,7 +11,7 @@ const LandingPage = () => {
           TRAVEL WITH YOUR FUR BABY
         </h1>
         <h2 className="mainpage-shop">
-          SHOP DEPAWTURE
+          <Link to='/new-arrivals' className="shop-depawture">SHOP DEPAWTURE</Link>
         </h2>
       </div>
     </section>
