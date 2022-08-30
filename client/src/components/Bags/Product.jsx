b455cbd0a726799ced47dfc3e434a4d61adede28
avacadok <avakuang8@gmail.com>
2022-08-29 19:53:57 -0700

    style product display on bag page with css grid

--- client/src/components/Bags/Product.jsx
+++ client/src/components/Bags/Product.jsx
@@ -6,18 +6,16 @@ const Product = (props) => {
 
   return (
     <>
-
       <section className="product-list">
         <div className="product-img">
           <img src={photos[0]} alt="product-img"></img>
         </div>
 
         <div className="product-title">{name}</div>
-        <div>{color}</div>
-        <div>${price/100}</div>
+        <div className="product-color">Color: {color}</div>
+        <div className="product-price">C ${price/100}</div>
 
       </section>
-
     </>
   )
 }
