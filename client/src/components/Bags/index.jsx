b6b62e3e61e63ad5db73eddea32b9d5c9e422d6f
avacadok <avakuang8@gmail.com>
2022-08-29 17:01:09 -0700

    display product on bag page and pass props for product details

--- client/src/components/Bags/index.jsx
+++ client/src/components/Bags/index.jsx
@@ -1,9 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import Product from "./Product";
 
 const Bags = () => {
-  return(
+  const [product, SetProduct] = useState({});
+
+  useEffect(() => {
+    axios.get('/api/bags')
+      .then((result) => {
+        SetProduct({ ...result.data })
+
+      })
+      .catch((err) => console.log("err", err));
+  }, []);
+  console.log("bags", product)
+
+  const ProductDetail = Object.values(product).map((product, index) => {
+    return (
+      <Product
+        key={index}
+        name={product.name}
+        color={product.color}
+        material={product.material}
+        description={product.description}
+        price={product.price_cents}
+        photos={product.list_of_pictures} />
+    )
+  })
+
+  return (
     <>
-    <h3 className="page-title">Bags</h3>
+      <h3 className="page-title">Bags</h3>
+      <div className="grid-container">
+        {ProductDetail}
+      </div>
     </>
   )
 }
