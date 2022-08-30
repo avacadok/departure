07d2f32efb2e7d6c197a08d86fbd038833bf9101
avacadok <avakuang8@gmail.com>
2022-08-29 20:50:33 -0700

    add padding

--- client/src/components/Bags/index.jsx
+++ client/src/components/Bags/index.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-import Product from "./Product";
+import Product from "../Product";
 
 const Bags = () => {
   const [product, SetProduct] = useState({});
