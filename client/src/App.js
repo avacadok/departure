37144bffb63af1e6f711d5270f0e40897fdc95b4
avacadok <avakuang8@gmail.com>
2022-08-30 22:06:42 -0700

    add route on app for Login page

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { Routes, Route, Link } from 'react-router-dom'
 import Bags from './components/Bags';
 import Accessories from './components/Accessories';
 import NewArrivals from './components/NewArrivals';
+import Login from './components/Login';
 
 function App() {
   const [newArrivals, setNewArrivals] = useState({});
@@ -21,33 +22,34 @@ function App() {
 
   useEffect(() => {
     Promise.all(endpoints.map((api) => axios.get(api)))
-    .then(([{data: newArrivals}, {data: bags}, {data:accessories}]) => {
-      setNewArrivals({...newArrivals})
-      SetBags({...bags})
-      setAccessories({...accessories})
-    })
+      .then(([{ data: newArrivals }, { data: bags }, { data: accessories }]) => {
+        setNewArrivals({ ...newArrivals })
+        SetBags({ ...bags })
+        setAccessories({ ...accessories })
+      })
   }, [])
 
   return (
     <div>
       <header>
-        <div className="logo"><Link to='/' className='logo-link'>D E P A W T U R E</Link></div>
+        <div className="logo"><Link to={'/'} className='logo-link'>D E P A W T U R E</Link></div>
         <nav>
           <ul className="nav_links" >
-            <li><Link to='/new-arrivals'>New Arrivals</Link></li>
-            <li><Link to='/bags'>Bags</Link></li>
-            <li><Link to='/accessories'>Accessories</Link></li>
+            <li><Link to={'/new-arrivals'}>New Arrivals</Link></li>
+            <li><Link to={'/bags'}>Bags</Link></li>
+            <li><Link to={'/accessories'}>Accessories</Link></li>
           </ul>
         </nav>
-        <a className="login" href="#"><button>Log in</button></a>
+        <Link to='/login' className='login-button'>Log in</Link>
       </header>
 
       <Routes>
 
         <Route path='/' element={<LandingPage />} />
-        <Route path='/new-arrivals' element={<NewArrivals newArrivals={newArrivals}/>} />
-        <Route path='/bags' element={<Bags bags={bags}/>} />
-        <Route path='/accessories' element={<Accessories accessories={accessories}/>} />
+        <Route path='/new-arrivals' element={<NewArrivals newArrivals={newArrivals} />} />
+        <Route path='/bags' element={<Bags bags={bags} />} />
+        <Route path='/accessories' element={<Accessories accessories={accessories} />} />
+        <Route path='/login' element={<Login />} />
 
       </Routes>
 
