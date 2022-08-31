5eb8190fb0be4afba47a7e7ab799a1e8ebd3f51f
avacadok <avakuang8@gmail.com>
2022-08-30 22:07:35 -0700

    add html for Login page

---
+++ client/src/components/Login.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Login = () => {
+
+  return (
+    <>
+      <div className="page-title">Hello Traveler</div>
+
+      <section className="login-section">
+        <div className="login-info">
+          <Link to={'/login'} className="user-login">Log in</Link>
+          <Link to={'/register'} className="user-signup">Sign Up</Link>
+        </div>
+
+        <div className="input-email">
+          <input type={'email'} placeholder="Email address" >
+          </input>
+        </div>
+
+        <div className="input-password">
+          <input type={'password'} placeholder="Password" >
+          </input>
+        </div>
+
+        <div className="confirm-button">
+          Log in
+        </div>
+
+      </section>
+    </>
+  )
+
+}
+
+export default Login;
