3fa1faf11484dee958fb32fd450ea9e5d31d8026
avacadok <avakuang8@gmail.com>
2022-09-06 15:35:44 -0700

    display product detail and style page

--- client/src/components/ProductDetail.jsx
+++ client/src/components/ProductDetail.jsx
@@ -15,13 +15,53 @@ const ProductDetail = (props) => {
   }
 
   return (
-    <>
-    <section className="product-detail">
+    <div className="product-info">
+      <section className="product-details">
+        <div className="product-detail-img">
+          <img src={products[productId].list_of_pictures[0]} >
+          </img>
+        </div>
 
-    </section>
-      <h3>{products[productId].name}</h3>
+        <div className="product-detail-info">
+          <div className="product-detail-name">
+            <h2>{products[productId].name}</h2>
+          </div>
 
-    </>
+          <div className="product-detail-price">
+            C ${products[productId].price_cents/100}
+          </div>
+
+          <div className="product-detail-color">
+            <p><b>Color:</b> {products[productId].color}</p>
+            <i className="fa-solid fa-circle"></i>
+          </div>
+
+          <div className="product-detail-cart">
+            Add to cart
+          </div>
+
+          <div className="description">
+            {products[productId].description}
+          </div>
+
+          <div className="product-detail-material">
+            <div>
+              <b>Material:</b> {products[productId].material}
+            </div>
+
+            <div>
+              <b>Size:</b> {products[productId].size}
+            </div>
+          </div>
+{/* 
+          <div>
+            <b>Size:</b> {products[productId].size}
+          </div> */}
+        </div>
+
+      </section>
+
+    </div>
   )
 }
 
